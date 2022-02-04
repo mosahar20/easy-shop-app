3528564ef3391a267a8d1956e95236bfdd87e9ce
mosahar20 <mosahar16@gmail.com>
2022-02-04 19:33:27 +0000

    updates

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -1,13 +1,13 @@
 import { Platform } from 'react-native'
 
-// let baseURL = 'https://easy-shop-server1000.herokuapp.com/api/v1/';
+let baseURL = 'https://easy-shop-server1000.herokuapp.com/api/v1/';
 
-let baseURL = '';
+// let baseURL = '';
 
-{Platform.OS == 'android'
-? baseURL = 'http://9b3e-2a02-c7d-31de-9000-1c9b-6aed-96e-ad47.ngrok.io/api/v1/'
-: baseURL = 'http://localhost:3000/api/v1/'
-}
+// {Platform.OS == 'android'
+// ? baseURL = 'http://bc97-2a02-c7d-31de-9000-5d8d-e3a7-f3b7-1e42.ngrok.io/api/v1/'
+// : baseURL = 'http://localhost:3000/api/v1/'
+// }
 
 
 
